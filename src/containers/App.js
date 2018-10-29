d16794a072d15c871f47ed28ceb6be24ae6ab784
Khaled Mohamed <khal3d.mohamed@gmail.com>
2018-10-29 15:08:14 +0200

    JSX

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import MyComponent from '../components/MyComponent';
+
+const JSXelement = <h1>Hello from JSX</h1>;
+const JSelement = React.createElement('h1', null, 'Hello from JS');
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MyComponent />
+        {JSXelement}
+        {JSelement}
       </div>
     );
   }
