cb66d71b5c4282a0ff1550402a29d5c91cb6e0a2
Khaled Mohamed <khal3d.mohamed@gmail.com>
2018-11-02 18:40:27 +0200

    practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    counter: 0
+  };
+
+  onIncrement = () => {
+    this.setState(prevState => ({ counter: prevState.counter + 1 }));
+  };
+
+  onDecrement = () => {
+    this.setState(prevState => ({ counter: prevState.counter - 1 }));
+  };
+
+  onInputChange = event => {
+    if (isNaN(+event.target.value)) {
+      return;
+    }
+    this.setState({ counter: +event.target.value });
+  };
+
   render() {
+    const { counter } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Counter App</h1>
+        <p>Counter: {counter}</p>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        <input value={counter} onChange={this.onInputChange} placeholder="Put Counter Value.." />
       </div>
     );
   }
